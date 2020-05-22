6128828a3a383d9bd7f1cd46a27b13dbf57a47af
Dejailson Silva <kauakarate@gmail.com>
2020-05-22 16:13:47 -0300

    primeiro commit

---
+++ Personagem.cs
@@ -0,0 +1,20 @@
+namespace PrimeiroPOO
+{
+    public class Personagem
+    {
+        
+        public string nome = "Frodo";
+
+        public int idade = 20;
+
+        public string raça = " Frodo é um Hobbit";
+
+        public string espada;
+
+
+        public string Atacar()
+        {
+            return "Frodo atacou!";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace PrimeiroPOO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Personagem frodo = new Personagem();
+           frodo.espada = "Ferruada";
+           
+           Console.WriteLine(frodo.nome);
+           Console.WriteLine(frodo.espada);
+           Console.WriteLine(frodo.raça);
+
+           Console.WriteLine(frodo.Atacar());
+
+          
+        }
+    }
+}
